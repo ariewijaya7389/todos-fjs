7109e287f64f7147a0d95aeba86903f8dfd8394e
Arie Wijaya <ariewijaya7389@gmail.com>
2019-11-14 10:01:30 +0800

    Fixes

--- app.js
+++ app.js
@@ -1,100 +1,117 @@
-var createError = require('http-errors');
-var express = require('express');
-var exphbs = require('express-handlebars');
-var mongoose = require('mongoose');
-var sassMiddleware = require('node-sass-middleware');
-var browserify = require('browserify-middleware');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var sassMiddleware = require("node-sass-middleware");
+var browserify = require("browserify-middleware");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var todosRouter = require('./routes/todos');
-
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var todosRouter = require("./routes/todos");
 
 var app = express();
 
-var todos = require('./routes/todos/index');
-var todosAPI = require('./routes/todos/api');
-var bodyParser = require('body-parser');
+var todos = require("./routes/todos/index");
+var todosAPI = require("./routes/todos/api");
+var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use('/todos', todos);
-app.use('/api/todos', todosAPI);
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use("/todos", todos);
+app.use("/api/todos", todosAPI);
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
 //Handlebars
-app.engine('hbs', exphbs({
-  extname: '.hbs',
-  defaultLayout: 'layout'
-}));
-app.set('view engine', 'hbs');
+app.engine(
+  "hbs",
+  exphbs({
+    extname: ".hbs",
+    defaultLayout: "layout"
+  })
+);
+app.set("view engine", "hbs");
 
 //Sass Middleware
-app.use(sassMiddleware({
-  src: __dirname + '/sass',
-  dest: __dirname + '/public',
-  debug: true,
-}));
+app.use(
+  sassMiddleware({
+    src: __dirname + "/sass",
+    dest: __dirname + "/public",
+    debug: true
+  })
+);
 
 //Browserify
 browserify.settings({
-  transform: ['hbsfy']
+  transform: ["hbsfy"]
 });
-app.get('/javascripts/bundle.js', browserify('./client/script.js'));
+app.get("/javascripts/bundle.js", browserify("./client/script.js"));
 
 //Mongoose
-var dbConnectionString = process.env.MONGODB_URI || 'mongodb://localhost';
-mongoose.connect(dbConnectionString + '/todos');
+var dbConnectionString = process.env.MONGODB_URI || "mongodb://localhost";
+mongoose.connect(dbConnectionString + "/todos");
 
 //Browser-sync
-if (app.get('env') == 'development') {
-  var browserSync = require('browser-sync');
+if (app.get("env") == "development") {
+  var browserSync = require("browser-sync");
   var config = {
-    files: ["public/**/*.{js.css}", "client/*.js", "sass/**/*.scss", "view/**/*.hbs"],
-    logLevel: 'debug',
+    files: [
+      "public/**/*.{js.css}",
+      "client/*.js",
+      "sass/**/*.scss",
+      "view/**/*.hbs"
+    ],
+    logLevel: "debug",
     logSnippet: false,
     reloadDelay: 3000,
     reloadOnRestart: true
-  }
+  };
   var bs = browserSync(config);
-  app.use(require('connect-browser-sync')(bs));
+  app.use(require("connect-browser-sync")(bs));
 }
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/fonts', express.static(path.join(__dirname, 'node_modules/bootstrap-sass/assets/fonts')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/fonts",
+  express.static(
+    path.join(__dirname, "node_modules/bootstrap-sass/assets/fonts")
+  )
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/todos', todosRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/todos", todosRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
